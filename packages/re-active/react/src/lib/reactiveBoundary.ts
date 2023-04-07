e815b4ea037b3bf9f3a43e0f65db2c0148b09650
kutlugsahin <kutlugsahin@gmail.com>
2023-04-07 18:14:36 +0300

    ReactiveBoundary type corrected

--- packages/re-active/react/src/lib/reactiveBoundary.ts
+++ packages/re-active/react/src/lib/reactiveBoundary.ts
@@ -5,13 +5,16 @@ import { createRenderComponentState, renderComponent } from './renderComponent';
 import { RenderResult } from './types';
 import { useReactiveProps } from './utils';
 
-type ReactiveBoundaryProps<T> = { data: T; children: (data: UnwrapRef<T>) => RenderResult } | { children: () => RenderResult };
-
-function ReactiveBoundaryComponent<T>(props: { data: T; children: (data: UnwrapRef<T>) => RenderResult }) {
+function ReactiveBoundaryComponent(props: { data: undefined; children: () => RenderResult }): ReturnType<FC>;
+function ReactiveBoundaryComponent<T>(props: {
+  data: T;
+  children: (data: UnwrapRef<T>) => RenderResult;
+}): ReturnType<FC>;
+function ReactiveBoundaryComponent<T>(props: { data?: T; children: (data?: UnwrapRef<T>) => RenderResult }) {
   const reactiveProps = useReactiveProps(props);
   return renderComponent(useRef(createRenderComponentState()).current, () =>
     reactiveProps.children(reactiveProps.data)
   ) as RenderResult;
 }
 
-export const ReactiveBoundary = memo(ReactiveBoundaryComponent) as FC<ReactiveBoundaryProps<any>>;
+export const ReactiveBoundary = memo(ReactiveBoundaryComponent) as typeof ReactiveBoundaryComponent;
